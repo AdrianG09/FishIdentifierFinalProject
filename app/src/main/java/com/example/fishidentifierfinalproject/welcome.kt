90107760fd4e223cbd99582266e3a4789effe126
AdrianG09 <gaidulaa@gmail.com>
2023-04-28 13:02:27 -0400

    4/28

--- app/src/main/java/com/example/fishidentifierfinalproject/welcome.kt
+++ app/src/main/java/com/example/fishidentifierfinalproject/welcome.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fishidentifierfinalproject.databinding.FragmentMainBinding
 
 class welcome : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
